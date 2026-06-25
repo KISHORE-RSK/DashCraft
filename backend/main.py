cc1f440d9ea33be8a4956130da28f98223083a76
KISHORE-RSK <kishore5jul2005@gmail.com>
2026-06-25 23:46:23 +0530

    Initial commit

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,13 @@ load_dotenv()
 
 app = FastAPI()
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class LoginRequest(BaseModel):
     username: str
